e23da27c11d1c7630c0c6d10717af9d684275090
Sergio Arnado <sergioarnado@gmail.com>
2023-12-17 00:27:03 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.eval01',
+  appName: 'eval01',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
